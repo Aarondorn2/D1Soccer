3bf31bff8450007bc9052e907cc287bd90c3a1ca
Heidorn <aarondorn2@gmail.com>
2017-10-26 15:35:33 -0400

    cleaned up styling on contact

--- app/components/content-container.js
+++ app/components/content-container.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+  willRender() {
+    $('html').addClass('page-bg').removeClass('main-bg'); //change background
+  }
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,42 +1,45 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
 
   setupController: function(controller) {
     controller.setProperties({
-      lat: 32.75494243654723,
-      lng: -86.8359375,
-      zoom: 4,
-    //   mapType: 'satellite', // Accepts 'roadmap', 'satellite', 'hybrid', or 'terrain'
-    //   showMapTypeControl: true,
-    //   clickableIcons: true,
-    //   draggable: true,
-    //   disableDefaultUI: false,
-    //   disableDoubleClickZoom: false,
-    //   scrollwheel: true,
-    //   showZoomControl: true,
-    //   showScaleControl: true
-    //   markers: [
-    //   {
-    //     id: 'unique-marker-id',  // Recommended
-    //     lat: 33.516674497188255, // Required
-    //     lng: -86.80091857910156, // Required
-    //     infoWindow: {
-    //       content: '<p>Birmingham</p>',
-    //       visible: true
-    //     },
-    //     click(event, marker) {},
-    //     rightclick(event, marker) {},
-    //     dblclick(event, marker) {},
-    //     mouseover(event, marker) {},
-    //     mouseout(event, marker) {},
-    //     mouseup(event, marker) {},
-    //     mousedown(event, marker) {},
-    //     drag(e, marker) {},
-    //     dragstart(e, marker) {},
-    //     dragend(e, marker) {}
-    //   }
-    // ]
+      lat: 35.035266,
+      lng: -85.149367,
+      zoom: 17,
+      mapType: 'satellite', // Accepts 'roadmap', 'satellite', 'hybrid', or 'terrain'
+      showMapTypeControl: true,
+      clickableIcons: true,
+      draggable: true,
+      disableDefaultUI: false,
+      disableDoubleClickZoom: false,
+      scrollwheel: true,
+      showZoomControl: true,
+      showScaleControl: true,
+      markers: [
+        {
+          id: 'D1 Chattanooga',  // Recommended
+          lat: 35.03489461, // Required
+          lng: -85.14845471, // Required
+          infoWindow: {
+            content: '<div style="text-align:center;"><h5>D1 Chattanooga</h5><p>7430 Commons Blvd, Chattanooga, TN 37421</p></div>',
+            visible: true
+          },
+          click(event, marker) {},
+          rightclick(event, marker) {},
+          dblclick(event, marker) {},
+          mouseover(event, marker) {},
+          mouseout(event, marker) {},
+          mouseup(event, marker) {},
+          mousedown(event, marker) {},
+          drag(e, marker) {},
+          dragstart(e, marker) {},
+          dragend(e, marker) {}
+        }
+      ]
+
+
     });
   }
 });
