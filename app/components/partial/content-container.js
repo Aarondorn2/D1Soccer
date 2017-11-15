c93c062591e776be01351a266b974b6427c312b0
Heidorn <aarondorn2@gmail.com>
2017-11-15 15:11:22 -0500

    adding slate theme for secure area

--- app/components/partial/content-container.js
+++ app/components/partial/content-container.js
@@ -4,9 +4,15 @@ import Ember from 'ember';
 export default Component.extend({
 
   willRender() {
-    Ember.$('html').addClass('page-bg').removeClass('main-bg'); //change background
+    if(this.get('theme') && this.get('theme') === "dark") {
+      Ember.$('html').addClass('dark-bg').removeClass('main-bg'); //change background
+    } else {
+      Ember.$('html').addClass('page-bg').removeClass('main-bg'); //change background
+    }
   },
   willClearRender() {
-      Ember.$('html').addClass('main-bg').removeClass('page-bg'); //change background
+      Ember.$('html').addClass('main-bg') //change background back to main
+      .removeClass('page-bg') //remove other backgrounds
+      .removeClass('dark-bg');
   }
 });
