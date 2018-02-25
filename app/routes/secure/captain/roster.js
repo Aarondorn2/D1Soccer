55b4f7887a9832986238bff4948739e2a034c950
Unknown <Aarondorn2@gmail.com>
2018-02-25 10:06:18 -0500

    added admin view to captain rosters

--- app/routes/secure/captain/roster.js
+++ app/routes/secure/captain/roster.js
@@ -1,7 +1,25 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
     model() {
-      return this.store.findAll('captainRoster');
-    }
+      return Ember.RSVP.hash({
+        teams: this.store.findAll('team'),
+        rosters: this.store.findAll('captainRoster'),
+        user: this.store.peekRecord('user', this.get('session.currentUser').userId)
+      });
+    },
+    afterModel(model) {
+      let teams = model.teams.sortBy('teamName');
+      let whitelistTeams = Array();
+      model.rosters.mapBy('teamName').forEach((teamName) => {
+        teams.forEach((team) => {
+          if (team.get('teamName') == teamName || team.get('teamName') == "Free Agents") {
+            whitelistTeams.push(team);
+          }
+        });
+      });
+      model.teams = whitelistTeams.uniqBy('teamName');
+    },
+
 });
