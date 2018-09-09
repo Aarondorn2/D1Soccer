b59f967d69e665a7725f12f12ca6fcaa4a32f859
Unknown <Aarondorn2@gmail.com>
2018-09-09 09:36:38 -0400

    Better schedule/standings

--- app/routes/league/standings.js
+++ app/routes/league/standings.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
+    imgur: Ember.inject.service(),
+
+    model() {
+      return this.get('imgur').getAlbum('oKmurVW');
+    }
 });
